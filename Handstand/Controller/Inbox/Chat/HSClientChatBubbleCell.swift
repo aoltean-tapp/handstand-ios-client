aaa2a219f7d2b68c072ee055cd39d3f2e1d979db
Andrei Oltean <andrei.oltean@tapptitude.com>
2018-02-13 13:21:23 +0200

    Create chat bubbles for both clients and trainers

---
+++ Handstand/Controller/Inbox/Chat/HSClientChatBubbleCell.swift
@@ -0,0 +1,20 @@
+//
+//  HSClientChatBubbleCell.swift
+//  Handstand
+//
+//  Created by Andrei Oltean on 2/13/18.
+//  Copyright © 2018 Handstand. All rights reserved.
+//
+
+import UIKit
+
+class HSClientChatBubbleCell: UICollectionViewCell {
+
+    @IBOutlet weak var clientMessageLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ Handstand/Controller/Inbox/Chat/HSTrainerChatBubbleCell.swift
@@ -0,0 +1,21 @@
+//
+//  HSTrainerChatBubbleCell.swift
+//  Handstand
+//
+//  Created by Andrei Oltean on 2/12/18.
+//  Copyright © 2018 Handstand. All rights reserved.
+//
+
+import UIKit
+
+class HSTrainerChatBubbleCell: UICollectionViewCell {
+
+    @IBOutlet weak var trainerImageView: UIImageView!
+    @IBOutlet weak var trainerMessageLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
